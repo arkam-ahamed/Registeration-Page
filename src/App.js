ec736492f85f5e79f1d934cfff671b35be0b946b
arkamahamed <arkamahamed88@gmail.com>
2022-01-22 01:17:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FaUserAlt } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { Route, Routes, Link } from "react-router-dom";
+import Signup from "./Signup";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+			</Routes>
+		</div>
+	);
 }
+
+const Login = () => (
+	<div className="main">
+		<div className="sub-main">
+			<div>
+				<h1>Login Page</h1>
+				<FaUserAlt style={{ fontSize: "50px", paddingBottom: "10px" }} />
+			</div>
+			<div>
+				<FaEnvelope className="inputDiv" />
+				<input type="text" placeholder="user name" className="name" />
+			</div>
+			<div className="second-input">
+				<FaLock className="inputDiv" style={{ color: "black" }} />
+
+				<input type="password" placeholder="password" className="name" />
+			</div>
+			<div className="login-button">
+				<button>Login</button>
+				<p className="link">
+					Don't have an account? <Link to="/signup">Sign Up Here</Link>
+				</p>
+			</div>
+		</div>
+	</div>
+);
 
 export default App;
---
+++ src/Signup.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./App.css";
+import { FaUserAlt } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa";
+
+import { Link } from "react-router-dom";
+
+function Signup() {
+	return (
+		<div className="main signupPage ">
+			<div className="sub-main signup">
+				<div>
+					<h1>Sign Up Page</h1>
+					<FaUserAlt style={{ fontSize: "50px", paddingBottom: "10px" }} />
+				</div>
+				<div className="mainInput">
+					<div>
+						<FaUserTie className="inputDiv" />
+						<input type="text" placeholder="Full name" className="name" />
+					</div>
+					<div>
+						<FaUserTie className="inputDiv" />
+						<input type="text" placeholder="user name" className="name" />
+					</div>
+					<div>
+						<FaEnvelope className="inputDiv" />
+						<input type="text" placeholder="Email" className="name" />
+					</div>
+					<div className="signup-password">
+						<FaLock className="inputDiv" style={{ color: "black" }} />
+
+						<input type="password" placeholder="password" className="name" />
+					</div>
+					<div className="confirm-password" style={{ paddingBottom: "15px" }}>
+						<FaLock className="inputDiv" />
+
+						<input
+							type="password"
+							placeholder="confirm password"
+							className="name"
+						/>
+					</div>
+				</div>
+				<div className="login-button">
+					<button>Sign up</button>
+					<p className="link">
+						Already have an account? <Link to="/">Log in</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Signup;
